ddcea587c8a7ae76b8d01106b3480c6d491f88e3
X <kanojia24.10@gmail.com>
2016-10-30 09:23:38 +0530

    Range download added.
    Script can now download Issues of certain range now. See the documentation for updated option description.

--- readcomic.py
+++ readcomic.py
@@ -9,7 +9,7 @@
 __author__      = "Xonshiz"
 __email__ = "Xonshiz@psychoticelites.com"
 __website__ = "http://www.psychoticelites.com"
-__version__ = "v2.1"
+__version__ = "v2.2"
 __description__ = "Downloads Issues from http://readcomiconline.to/"
 __copyright__ = "None!"
 
@@ -26,13 +26,13 @@ __copyright__ = "None!"
 # 4.) Skips the file if it already exists in the path. 														#
 # 5.) Option to choose Qulity of Images. 																	#
 # 6.) Option to download Latest or Older releases.	 														#
+# 7.) Option to download particular Issues from a series. 													#
 #																											#
 #############################################################################################################
 # 										FUTURE FEATURES :													#
 #############################################################################################################
 #																											#
-# 1.) Option to download particular Issues from a series. 													#
-# 2.) Error Log File creation. 																				#
+# 1.) Error Log File creation. 																				#
 #																											#
 #############################################################################################################
 # 										CHANGELOG :															#
@@ -47,6 +47,7 @@ __copyright__ = "None!"
 # 6.) Script won't break on HTTP errors.(Thanks to @Efreak)	 												#
 # 6.) Weird File name Fix and some Minor Bug Fix	 														#
 # 7.) Annual Issues are now downloadable,			 														#
+# 8.) Option to download particular Issues from a series. 													#
 #																											#
 #############################################################################################################	
 
@@ -82,6 +83,8 @@ sys.setdefaultencoding('utf-8')
 
 '''
 
+
+
 def Url_Fetching():
 	
 	print '\n'
@@ -109,7 +112,7 @@ def Url_Fetching():
 			match = found.groupdict()
 			if match['issue']:
 				#print('Issue url: {}'.format(match))
-				Edited_Url = str(Series_URL)+'&readType=1'
+				Edited_Url = str(Series_URL)+'?&readType=1'
 				url = str(Edited_Url)
 				Quality = Settings_Reader()
 				Single_Issue(url,Quality)
@@ -128,7 +131,7 @@ def Url_Fetching():
 			
 			if match['issue']:
 				#print('Issue url: {}'.format(match))
-				Edited_Url = str(Series_URL)+'&readType=1'
+				Edited_Url = str(Series_URL)+'?&readType=1'
 				url = str(Edited_Url)
 				Quality = Settings_Reader()
 				Single_Issue(url,Quality)
@@ -286,64 +289,107 @@ def Single_Issue(url,Quality):
 def Whole_Series(driver,url,Quality):
 	#print '\nDownloading Whole Series'
 	#print url
-	
-	driver.get(url)
-	try:
-		print 'Bypassing the check. Wait for a few seconds please.'
-		element = WebDriverWait(driver, 10).until(
-			EC.presence_of_element_located((By.ID, "stSegmentFrame"))
-		)
-		#print 'Downloading the whole page! Will take some time, please don\'t close this script...\n'
-		#print 'I\'ve waited long enough'
-	except Exception, e:
-		#raise e
-		driver.save_screenshot('Whole_exception.png')
-		print e
-		pass
-	finally:
-		Source_Code = driver.page_source
 
-		soure_file = open('source2.txt','w')
-		soure_file.write(Source_Code)
-		soure_file.flush()
-		soure_file.close()
+	Range_Input = str(raw_input("Enter The Range : ")).strip()
+
+	
+
+	if Range_Input.upper() not in ["NONE","NULL","ALL"]:
+
+		Range_Splitter = Range_Input.split("-")
+		Starting_Range = int(str(Range_Splitter[0]).strip())
+		Ending_Range = int(str(Range_Splitter[1]).strip())
+
+		Url_Spliiter = url.split("/Comic/")
+		Extra_Remover = str(Url_Spliiter[1]).replace('/','')
+		#print "Extra remover : ",Extra_Remover
+		Show_Name = str(Extra_Remover).strip()
+		Type_Input = str(raw_input("Annual or Issue? : ")).strip()
+
+		if Type_Input.upper() in ["ANNUAL"]:
+			Type = "Annual"
+		if Type_Input.upper() in ["ISSUE"]:
+			Type = "Issue"
+
+		for x in range(Starting_Range,Ending_Range+1):
+			#print x
+			#Example URL = http://readcomiconline.to/Comic/Injustice-Gods-Among-Us-I/Issue-36?
+			url = "http://readcomiconline.to/Comic/"+Show_Name+"/{}-".format(Type)+str(x)+"?&readType=1"
+			#print url
+			Single_Issue(url,Quality)
+
+	if Range_Input.upper() in ["NONE","NULL","ALL"]:
+
+		driver.get(url)
+		try:
+			print 'Bypassing the check. Wait for a few seconds please.'
+			element = WebDriverWait(driver, 10).until(
+				EC.presence_of_element_located((By.ID, "stSegmentFrame"))
+			)
+			#print 'Downloading the whole page! Will take some time, please don\'t close this script...\n'
+			#print 'I\'ve waited long enough'
+		except Exception, e:
+			#raise e
+			driver.save_screenshot('Whole_exception.png')
+			print e
+			pass
+		finally:
+			Source_Code = driver.page_source
+
+			soure_file = open('source2.txt','w')
+			soure_file.write(Source_Code)
+			soure_file.flush()
+			soure_file.close()
+			driver.quit()
+	
 		driver.quit()
+		try:
+			os.remove('.Temp_File') #Removing this because I need to check if there's a file with this name or not. Because if the file exists, then it'll APPEND to the older script and it'll download old + new comics
+		except Exception, e:
+			#raise e
+			pass
+
 	
-	driver.quit()
-	try:
-		os.remove('.Temp_File') #Removing this because I need to check if there's a file with this name or not. Because if the file exists, then it'll APPEND to the older script and it'll download old + new comics
-	except Exception, e:
-		#raise e
-		pass
-	
-	
-	with open('source2.txt') as searchfile:
-		with open('.Temp_File','a') as Temp_File_variable:
-			for line in searchfile:
-				left,sep,right = line.partition('title="Read ') # Extra Space to make it more precise and drop the readcomiconline.to link
-				if sep:
-					OG_Title = left.split('"')
-					#print OG_Title
-					#print OG_Title[1]
-					Raw_Issue_Links = OG_Title[1].strip()
-					Issue_Links = 'http://readcomiconline.to'+str(Raw_Issue_Links)
-					#print Issue_Links,' #'
-					Temp_File_variable.write(str(Issue_Links)+'\n')
-					Temp_File_variable.flush()
-		Temp_File_variable.close()
+		with open('source2.txt') as searchfile:
+			with open('.Temp_File','a') as Temp_File_variable:
+				for line in searchfile:
+					left,sep,right = line.partition('title="Read ') # Extra Space to make it more precise and drop the readcomiconline.to link
+					if sep:
+						OG_Title = left.split('"')
+						#print OG_Title
+						#print OG_Title[1]
+						Raw_Issue_Links = OG_Title[1].strip()
+						Issue_Links = 'http://readcomiconline.to'+str(Raw_Issue_Links)
+						#print Issue_Links,' #'
+						Temp_File_variable.write(str(Issue_Links)+'\n')
+						Temp_File_variable.flush()
+			Temp_File_variable.close()
 
 
-	with open(".Temp_File","r") as input:
-		with open(".Temp_File2","wb") as output: 
-			for line in input:
-				if line != "http://readcomiconline.to../../Content/images/bullet.png":
-					output.write(line)
-					#print line
+		with open(".Temp_File","r") as input:
+			with open(".Temp_File2","wb") as output: 
+				for line in input:
+					if line != "http://readcomiconline.to../../Content/images/bullet.png":
+						output.write(line)
+						#print line
 
 		
-	if Quality[1] in ['LATEST']:
-		with open('.Temp_File','r') as Link_File:
-			for line in Link_File:
+		if Quality[1] in ['LATEST']:
+			with open('.Temp_File','r') as Link_File:
+				for line in Link_File:
+					bs_link = 'http://readcomiconline.to../../Content/images/bullet.png'
+					if bs_link in line:
+						#print 'line'
+						pass
+					else :
+						url = str(line)
+						#print url
+						Single_Issue(url,Quality)
+
+	
+		elif Quality[1] in ['OLD']:
+			for line in reversed(open('.Temp_File').readlines()):
+				#print line
 				bs_link = 'http://readcomiconline.to../../Content/images/bullet.png'
 				if bs_link in line:
 					#print 'line'
@@ -353,25 +399,13 @@ def Whole_Series(driver,url,Quality):
 					#print url
 					Single_Issue(url,Quality)
 
-	
-	elif Quality[1] in ['OLD']:
-		for line in reversed(open('.Temp_File').readlines()):
-			#print line
-			bs_link = 'http://readcomiconline.to../../Content/images/bullet.png'
-			if bs_link in line:
-				#print 'line'
-				pass
-			else :
-				url = str(line)
-				#print url
-				Single_Issue(url,Quality)
-
-
-	os.remove('source2.txt')		
+		'''
+		os.remove('source2.txt')		
+		'''
 		
 
 	
-	#Remove the .temp_fle so that it doesn't append to the old data!	
+		#Remove the .temp_fle so that it doesn't append to the old data!	
 
 def Settings_Reader():
 
@@ -381,6 +415,15 @@ def Settings_Reader():
 	#print config.sections()
 	Quality = config.get('ScriptSettings', 'Quality')
 	Order = config.get('ScriptSettings', 'Order')
+	#Chapters_Range = config.get('ScriptSettings', 'Chapters(Range)')
+
+	#Chap_Range_Breaker = str(Chapters_Range).split('-')
+	#Start_Number = str(Chap_Range_Breaker[0]).strip()
+	#End_Number = str(Chap_Range_Breaker[1]).strip()
+	#print Chapters_Range
+	#print Start_Number
+	#print End_Number
+
 	#print Quality,Order
 
 	if Quality.upper() in ['LQ','LOW','LOW QUALITY']:
@@ -396,10 +439,11 @@ def Settings_Reader():
 	elif Order.upper() in ['OLD','INITIAL']:
 		#print 'Older Issues'
 		Order = 'OLD'
-
+	
 	return (Quality,Order)
 
 try:
+	#Chap_Checker()
 	Url_Fetching()
 	#Settings_Reader()
 except Exception, e:
